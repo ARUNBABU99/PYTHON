545243ac1d18757b0635d3380c6dadd5f9b07871
ARUNBABU99 <arunbabu994724@gmail.com>
2022-07-22 00:30:09 +0530

    now list as user input can be accepted

--- trail.py
+++ trail.py
@@ -1,5 +1,3 @@
-x=int(input("enter your age"))
-if x > 18:
-    if x >21:
-        print ("you can have a beer too")
-    print ("you are eligible for voting")
+x= 10 ,20 ,20
+for i in  x:
+    print (i)
