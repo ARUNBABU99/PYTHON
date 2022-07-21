27d1248d0a51004e84e46ee4fe0c9e27be14ad74
ARUNBABU99 <arunbabu994724@gmail.com>
2022-07-21 23:31:23 +0530

    days caclulated using nested if in a function

--- test.py
+++ test.py
@@ -1,7 +1,52 @@
-from modules import *
 
 
-add(5,7)
-sub(10,2)
 
-print ("hey bro this is test")
+from ast import Num
+
+
+number_of_units=24
+units= "hours"
+
+def time_try(days):
+        # print(days>0)
+        # if days > 0 : 
+            
+    return ( f"{days} days are {days*number_of_units} {units}" )
+        # elif days ==0:
+        #     return ("you entered zero")
+        # else:
+        #     return("enter a +ve val")   
+        # print("please enter a digit")
+
+
+
+# def validate():
+#     if x.isdigit():
+#         x=int(X)
+#         z=time_try(x)   
+#         print(z)
+#     else:
+#         print("enter a number")
+
+
+def check():
+    # x=input("enter the no of days: \n")
+    if num1.isdigit():
+        x=int(num1)
+        if x > 0:
+            # x=int(xb)
+            z=time_try(x)   
+            print(z)
+        elif x == 0:
+            print ("yu enetered 0")
+        elif x  < 0:
+            print ("enter +ve val")
+    else:
+        print("enter a number")
+
+num1=input("enter the no of days: \n")
+check()
+
+
+
+# validate()
